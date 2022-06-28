6b4f06c2b8288444965104a107c2ad8849255708
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-06-28 09:05:56 +0700

    addd code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # F8-JS-Basic
+
+Array.forEach(function(item, index){}) => loop 
+Array.everey(function(item){ return boolean }) =>  check all items with condition
+Array.some(function(item){ return boolean }) =>  check a item with condition
+Array.find(function(item){ return item }) =>  check a item with condition
+Array.filter(function(item){ return items }) =>  check items with condition
--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('testinggg');
+var courses = [
+    {
+        id: 1,
+        name: 'PHP',
+        coin: 1000
+    },
+    {
+        id: 2,
+        name: 'Laravel',
+        coin: 1230
+    },
+    {
+        id: 3,
+        name: 'JS',
+        coin: 1000
+    },
+    {
+        id: 4,
+        name: 'Python',
+        coin: 1000
+    },
+]
+
+var isFree = courses.find(function(course, index) {
+    console.log(index);
+    return course.coin === 1000
+})
+
+console.log(isFree);
