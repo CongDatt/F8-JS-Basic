1992b765c1b3858b311fe35b37bc499154e04636
LAPTOP-CONGDAT\tranc <trancongdat21012000@gmail.com>
2022-07-03 15:21:17 +0700

    promise

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # F8-JS-Basic
+- JSON (JavaScript Object Notation): là một định dạng dữ liệu
+- JSON: Number, Boolean, Null, Array, Object.
+- JSON -> JS Type: JSON.parse()
+- JS Type -> JSON: JSON.Stringify()
+
+- Một promise sẽ có 3 trạng thái :
+    Pending (đang xử lý): khi new promise
+    Fulfilled (đã hoàn thành): khi resolve 
+    Rejected (đã bị từ chối): khi reject 
--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log('testinggg');
+var a = 1;
+var b = 1;
+
+var prommise = new Promise(function(resolve, reject){
+    if(a > b) {
+        resolve()
+    }
+    else {
+        reject()
+    }
+})
+
+prommise
+.then(function() {
+    console.log('thanh cong');
+})
+.catch(function(){
+    console.log('that bai !!');
+})
+.finally(function() {
+    console.log('Done !!');
+})
+
+
+var prommise1 = new Promise(
+    function(resolve) {
+        setTimeout(function(){
+            resolve([1,2])
+        },1000)
+    }
+)
+var prommise2 = new Promise(
+    function(resolve) {
+        setTimeout(function(){
+            resolve([3,4])
+        },5000)
+    }
+)
+
+Promise.all([prommise1,prommise2])
+.then(function(result){
+    console.log(result);
+})
